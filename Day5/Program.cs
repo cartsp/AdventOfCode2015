72a2a8efdd66bf3fb31dfeec71b027744ff4fc90
Liam Boyle <ltboyle@gmail.com>
2022-12-05 23:53:18 +0000

    regex for part 2 day 5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -6,7 +6,12 @@ var part1 = inputs
     .Select(NiceOrNaughty)
     .Count(a => a);
 
+var part2 = inputs
+    .Select(NiceOrNaughtyV2)
+    .Count(a => a);
+
 Console.WriteLine(part1);
+Console.WriteLine(part2);
 
 bool NiceOrNaughty(string phrase)
 {
@@ -20,4 +25,10 @@ bool NiceOrNaughty(string phrase)
 
     return Regex.Match(phrase, @"[aeiou].*[aeiou].*[aeiou]").Success 
            && Regex.Match(phrase, @"(.)\1{1,}").Success;
+}
+
+bool NiceOrNaughtyV2(string phrase)
+{
+    return Regex.Match(phrase, @"(\w\w).*?\1").Success 
+           && Regex.Match(phrase, @"(\w)\w\1").Success;
 }
