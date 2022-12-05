a631d818f1c6bc3461d7bf0e063350db157de0ab
Liam Boyle <ltboyle@gmail.com>
2022-12-05 20:28:25 +0000

    Part1 complete

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var inputs = File.ReadAllLines("day5input.txt");
+
+var part1 = inputs
+    .Select(NiceOrNaughty)
+    .Count(a => a);
+
+Console.WriteLine(part1);
+
+bool NiceOrNaughty(string phrase)
+{
+    if (phrase.Contains("ab") ||
+        phrase.Contains("cd") ||
+        phrase.Contains("pq") ||
+        phrase.Contains("xy"))
+    {
+        return false;
+    }
+
+    return Regex.Match(phrase, @"[aeiou].*[aeiou].*[aeiou]").Success 
+           && Regex.Match(phrase, @"(.)\1{1,}").Success;
+}
